b031fdda11cddc7b2bc539e8c631f916631eff41
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-01-29 11:15:59 -0500

    created alpha version of program

--- GC_Lab_3.2/Program.cs
+++ GC_Lab_3.2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace GC_Lab_3._2
 {
@@ -6,7 +8,158 @@ namespace GC_Lab_3._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, double> menuItems = PopulateMenuItems();
+
+            Console.WriteLine("=============================================");
+            Console.WriteLine("== Welcome to Nic's Happy Food and Stuff!! ==");
+            Console.WriteLine("=============================================");
+            Console.WriteLine();
+
+            PrintMenuItems(menuItems);
+
+            FillCart(menuItems, out List<string> itemCart, out List<double> itemPriceCart);
+
+            Console.WriteLine();
+            Console.WriteLine("Looks like you are all done shopping.");
+            Console.WriteLine("Lets take a quick look at what you ordered.");
+            PrintCart(itemCart, itemPriceCart);
+            double avePrice = GetAvePrice(itemPriceCart);
+            Console.WriteLine($"Exellent, your average item price was ${avePrice:N2}.");
+            Console.WriteLine("Have a fantastic day!");
+
+        }
+
+        private static double GetAvePrice(List<double> itemPriceCart)
+        {
+            var total = 0.0;
+            
+            foreach(var itemPrice in itemPriceCart)
+            {
+
+                total += itemPrice;
+            }
+
+            return total / itemPriceCart.Count;
+        }
+
+        private static void FillCart(Dictionary<string, double> menuItems, out List<string> itemCart, out List<double> itemPriceCart)
+        {
+            PrintHelpMenu();
+            itemCart = new ArrayList();
+            itemPriceCart = new ArrayList();
+            var done = false;
+            do
+            {
+                Console.Write("\nWhat item would you like to add to your cart: ");
+                string userInput = Console.ReadLine().ToLower();
+
+                if (userInput.Contains("done"))
+                {
+                    done = true;
+                }
+                else if (userInput.Contains("menu"))
+                {
+                    PrintMenuItems(menuItems);
+                } 
+                else if (userInput.Contains("cart"))
+                {
+                    PrintCart(itemCart, itemPriceCart);
+                }
+                else if (userInput.Contains("help"))
+                {
+                    PrintHelpMenu();
+                }
+                else if (TryGetMenuItem(userInput, menuItems, out string item, out double price))
+                {
+                    itemCart.Add(item);
+                    itemPriceCart.Add(price);
+                }
+                else
+                {
+                    Console.WriteLine("Could not find desired item.");
+                    PrintHelpMenu();
+                }
+
+            } while (!done);
+        }
+
+        private static void PrintCart(List<string> itemCart, List<double> itemPriceCart)
+        {
+            if(itemCart.Count != itemPriceCart.Count)
+            {
+                throw new ArgumentException("ERROR: item cart and item price cart are out of sync");
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"--{"Cart Items",15} | {"Price",-7} --");
+            Console.WriteLine("------------------------------");
+            for (var i = 0; i < itemCart.Count; i++)
+            {
+                Console.WriteLine($"--{itemCart[i],15} | ${itemPriceCart[i],-7}--");
+                Console.WriteLine("------------------------------");
+            }
+
+            if (itemCart.Count == 0)
+            {
+                Console.WriteLine("--      Cart is empty       --");
+                Console.WriteLine("------------------------------");
+            }
+            Console.WriteLine();
+        }
+
+        private static bool TryGetMenuItem(string desiredItem, Dictionary<string, double> menuItems, out string item, out double price)
+        {
+            item = string.Empty;
+            price = 0.0;
+            foreach(var menuItem in menuItems)
+            {
+                if (menuItem.Key.Contains(desiredItem))
+                {
+                    item = menuItem.Key;
+                    price = menuItem.Value;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static void PrintHelpMenu()
+        {
+            Console.WriteLine("When choosing an item off the list type the exact name.");
+            Console.WriteLine("To complete your order type \'done\'");
+            Console.WriteLine("To see the menu items list again type \'menu\'");
+            Console.WriteLine("To see a list of items in your cart type \'cart\'");
+            Console.WriteLine("To see the commands type \'help\'");
+
+        }
+
+        static Dictionary<string, double> PopulateMenuItems()
+        {
+            var menuItems = new Dictionary<string, double>();
+            menuItems.Add("apple", 0.99);
+            menuItems.Add("banana", 0.59);
+            menuItems.Add("cauliflower", 1.59);
+            menuItems.Add("dragonfruit", 2.19);
+            menuItems.Add("elderberry", 1.79);
+            menuItems.Add("figs", 2.09);
+            menuItems.Add("grapefruit", 1.99);
+            menuItems.Add("honeydew", 3.49);
+            return menuItems;
+        }
+
+        static void PrintMenuItems(Dictionary<string, double> menuItems)
+        {
+            
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"--{"Menu Items",15} | {"Price",-7} --");
+            Console.WriteLine("------------------------------");
+            foreach (var menuItem in menuItems)
+            {
+                Console.WriteLine($"--{menuItem.Key,15} | ${menuItem.Value,-7}--");
+                Console.WriteLine("------------------------------");
+            }
+            Console.WriteLine();
         }
     }
 }
